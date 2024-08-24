8d7b86b03fecb35871f49f6843cf0ee6d215967d
gunahy <freshdv@mail.ru>
2024-08-24 18:49:03 +0500

    add Class Department

---
+++ Lesson631/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson631
+{
+    class Company
+    {
+        public string Type;
+        public string Name;
+    }
+
+    class Department
+    {
+        public Company Company;
+        public City City;
+    }
+
+    class City
+    {
+        public string Name;
+    }
+}
--- Lesson631/Program.cs
+++ Lesson631/Program.cs
@@ -10,6 +10,19 @@ namespace Lesson631
     {
         static void Main(string[] args)
         {
+            var department = GetCurrentDepartment();
+            Console.ReadKey();  
+        }
+        static Department GetCurrentDepartment()
+        {
+            var department = new Department();
+
+            if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
+            {
+                Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+            }
+            return department;
         }
     }
+
 }
